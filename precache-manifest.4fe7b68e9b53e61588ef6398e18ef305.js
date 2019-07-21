ff923f7fe848816fd8a78c11c08e4d89a9ce0309
Chris Chuang <chrischuang0421@gmail.com>
2019-07-21 20:52:48 +0800

    Updates

---
+++ precache-manifest.4fe7b68e9b53e61588ef6398e18ef305.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67229201b1f894b797398f530082ae1e",
+    "url": "/f2e2019_01_pomodoro/index.html"
+  },
+  {
+    "revision": "8ba13aaeea12fff299f3",
+    "url": "/f2e2019_01_pomodoro/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "876606c3289bc6fbdbb4",
+    "url": "/f2e2019_01_pomodoro/static/css/main.0805091b.chunk.css"
+  },
+  {
+    "revision": "8ba13aaeea12fff299f3",
+    "url": "/f2e2019_01_pomodoro/static/js/2.ae799bce.chunk.js"
+  },
+  {
+    "revision": "876606c3289bc6fbdbb4",
+    "url": "/f2e2019_01_pomodoro/static/js/main.94ce064c.chunk.js"
+  },
+  {
+    "revision": "f73bf87454b2dd2d9e2e",
+    "url": "/f2e2019_01_pomodoro/static/js/runtime~main.88525639.js"
+  },
+  {
+    "revision": "a11bce7d780a80b3ef04b28d92318e5a",
+    "url": "/f2e2019_01_pomodoro/static/media/Group 20.a11bce7d.svg"
+  },
+  {
+    "revision": "1045b181540c3f49184c75cbf2f79c5a",
+    "url": "/f2e2019_01_pomodoro/static/media/TaipeiSansTCBeta-Bold.1045b181.ttf"
+  },
+  {
+    "revision": "99eb32fa6433eb8c3144f6cc2b6867d8",
+    "url": "/f2e2019_01_pomodoro/static/media/TaipeiSansTCBeta-Light.99eb32fa.ttf"
+  },
+  {
+    "revision": "7cf11eedfd307ceae0e05fbd74fbde60",
+    "url": "/f2e2019_01_pomodoro/static/media/TaipeiSansTCBeta-Regular.7cf11eed.ttf"
+  }
+]);
